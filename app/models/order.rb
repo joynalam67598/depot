a6e39ed2cb08be340c9cae0777d90b7fb428af52
joynalam67598 <joynal67598@gmail.com>
2022-06-07 10:32:54 +0600

    add - capturing form validation and atom feed aggregator

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,17 +1,19 @@
 class Order < ApplicationRecord
-    has_many:line_items,dependent: :destroy
-
-    enum pay_type: {
-    "Check" => 0,
-    "Credit card"    => 1,
-    "Purchase order" => 2
-    }
-    validates :name, :address, :email, presence: true
-    validates :pay_type, inclusion: pay_types.keys
-    def add_line_items_from_cart(cart)
-        cart.line_items.each do |item|
-        item.cart_id = nil
-        line_items << item #add item to line item "<<"
-        end
+  has_many :line_items, dependent: :destroy
+  has_many :orders, through: :line_items
+  # product has many orders through its existing line items
+  # relationship
+  enum pay_type: {
+    'Check' => 0,
+    'Credit card' => 1,
+    'Purchase order' => 2
+  }
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: pay_types.keys
+  def add_line_items_from_cart(cart)
+    cart.line_items.each do |item|
+      item.cart_id = nil
+      line_items << item # add item to line item "<<"
     end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :line_items
   resources :carts
   root 'store#index', as: :store_index
-  resources :products
+  resources :products do
+    get :who_brought, on: :member
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
